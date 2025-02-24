f50987c2f6dce8edd15f07683a9577d5c715fcc2
kite <sgwgkaito@gmail.com>
2025-02-24 17:38:38 +0900

    use relearn

---
+++ content/posts/tips1.md
@@ -0,0 +1,7 @@
+---
+title: "Tips1"
+date: 2025-02-24T15:51:02+09:00
+draft: false
+---
+
+The second article.
---
+++ public/searchindex.en.js
@@ -0,0 +1,74 @@
+var relearn_searchindex = [
+  {
+    "breadcrumb": "Dummy site (Relearn)",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Articles",
+    "uri": "/articles/index.html"
+  },
+  {
+    "breadcrumb": "Dummy site (Relearn)",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Articles",
+    "uri": "/posts/index.html"
+  },
+  {
+    "breadcrumb": "",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Dummy site (Relearn)",
+    "uri": "/index.html"
+  },
+  {
+    "breadcrumb": "Dummy site (Relearn) \u003e  Articles",
+    "content": "The second article.",
+    "description": "The second article.",
+    "tags": [],
+    "title": "Tips1",
+    "uri": "/articles/tips1/index.html"
+  },
+  {
+    "breadcrumb": "Dummy site (Relearn) \u003e  Articles",
+    "content": "The second article.",
+    "description": "The second article.",
+    "tags": [],
+    "title": "Tips1",
+    "uri": "/posts/tips1/index.html"
+  },
+  {
+    "breadcrumb": "Dummy site (Relearn) \u003e  Articles",
+    "content": "Introduction This article is the first sample of a dummy website created by Hugo.",
+    "description": "Introduction This article is the first sample of a dummy website created by Hugo.",
+    "tags": [],
+    "title": "Intro",
+    "uri": "/articles/intro/index.html"
+  },
+  {
+    "breadcrumb": "Dummy site (Relearn) \u003e  Articles",
+    "content": "Introduction This article is the first sample of a dummy website created by Hugo.",
+    "description": "Introduction This article is the first sample of a dummy website created by Hugo.",
+    "tags": [],
+    "title": "Intro",
+    "uri": "/posts/intro/index.html"
+  },
+  {
+    "breadcrumb": "Dummy site (Relearn)",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Categories",
+    "uri": "/categories/index.html"
+  },
+  {
+    "breadcrumb": "Dummy site (Relearn)",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Tags",
+    "uri": "/tags/index.html"
+  }
+]
